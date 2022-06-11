ee6075f60cd7f23c89be92b7dd5659a07f540e4c
Jacob Meyer <jacobpmeyer87@gmail.com>
2022-06-11 14:48:36 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello, there")
+
+// switch the hotkeys for navigating between term and editor
+
+// Globals
+console.log(global)
+console.log(__dirname)
+console.log(__filename)
+
+// Scott says this might be the most helpful global for node.js
+// Shows lots of information, including environment variables
+console.log(process)
